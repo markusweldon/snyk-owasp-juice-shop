f70e0cbabab87cd9ca40763f307b268e07d25fdb
Markus Weldon <markus.weldon@gmail.com>
2025-02-13 13:28:03 -0800

    my newest feature

--- routes/profileImageUrlUpload.js
+++ routes/profileImageUrlUpload.js
@@ -15,6 +15,10 @@ module.exports = function profileImageUrlUpload () {
       const url = req.body.imageUrl
       if (url.match(/(.)*solve\/challenges\/server-side(.)*/) !== null) req.app.locals.abused_ssrf_bug = true
       const loggedInUser = insecurity.authenticatedUsers.get(req.cookies.token)
+
+      if (url.match(/(.)*solve\/challenges\/server-side(.)*/) !== null) req.app.locals.abused_ssrf_bug = true
+      const loggedInUser = insecurity.authenticatedUsers.get(req.cookies.token)
+
       if (loggedInUser) {
         const imageRequest = request
           .get(url)
